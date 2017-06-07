b972d77e8b4824baf64402c52782703f21d0d968
Nibir Bora <nbora@usc.edu>
2017-06-07 00:21:57 -0700

    using functions to abstract away list ops in timeline example

--- 1-timeline/share.py
+++ 1-timeline/share.py
@@ -5,10 +5,12 @@ from redis import Redis
 
 MAX_POSTS = 5
 
-handle = sys.argv[1]
-message = sys.argv[2]
 
-r = Redis()
+def share(handle, message):
+    r = Redis()
 
-r.lpush(handle, message)
-r.ltrim(handle, 0, MAX_POSTS - 1)
+    r.lpush(handle, message)
+    r.ltrim(handle, 0, MAX_POSTS - 1)
+
+
+share(sys.argv[1], sys.argv[2])
--- 1-timeline/view.py
+++ 1-timeline/view.py
@@ -4,17 +4,18 @@ import time
 
 from redis import Redis
 
-handle = sys.argv[1]
 
-r = Redis()
+def view(handle):
+    r = Redis()
 
-while True:
-    os.system('clear')
     print('{}\'s timeline...\n'.format(handle))
-
     items = r.lrange(handle, 0, -1)
 
     for item in items:
         print(item)
 
+
+while True:
+    os.system('clear')
+    view(sys.argv[1])
     time.sleep(1)
